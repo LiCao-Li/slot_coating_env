aa30238935f840518d052ee0bd2704e090f0b803
Li Cao <licao@uchicago.edu>
2021-08-25 16:20:30 -0500

    modified env define

--- slot_coating_env/envs/SlotDie_env.py
+++ slot_coating_env/envs/SlotDie_env.py
@@ -4,7 +4,8 @@ from gym.utils import seeding
 
 Class SlotDie(gym.Env):
 
-    """slot_die coating process
+    """
+    slot_die coating process
     The goal of this algorithm is to max the rewards among 100 independent coating experiments by using a certain type of liquid
     determinied values for a certain type of liquid:
     - ink viscosity
@@ -37,36 +38,35 @@ Class SlotDie(gym.Env):
 	- a,b_l,b_s,modified_CA, ambient pressure
 
 	help function:
-	- check_function(inputs_from_actions): return x_u values
+	- judge(inputs_from_actions): return x_u values
 
     The rewards is estimated by x_u values, it depicts the distance between x_u and x_f
 
     Ideally an agent will be able to recognise actions that lead to a higher reward and
     increase the rate in which choose that direction until the reward reaches
     its maximum 
+
     """
 	def __init__(self):
 	# self.action_space, self_observation_space
 	
-    self.experiments_count = 0
-    self.experiments_max = 100
-    self.observation = 0
+    	self.experiments_count = 0
+    	self.experiments_max = 100
+    	self.observation = 0
 
-    self.x_f = 500
-    self.Lu = 500
+    	self.x_f = 500
+    	self.Lu = 500
     # liquid property
-    self.viscosity = 23
-    self.density = 1210
-    self.n = 1
-    self.m = 0.045
-    self.surface = 0.066
+    	self.viscosity = 23
+    	self.density = 1210
+    	self.n = 1
+    	self.m = 0.045
+    	self.surface = 0.066
     # variables
     # states value are liquid positions
-	self.observation_space = spaces.Box(low = np.array([60,120]), 
-											high = np.array([120,160]), dtype=np.uint8)
+		self.observation_space = spaces.Box(low = np.array([60,120]),high = np.array([120,160]), dtype=np.uint8)
 	# action values are web_speed, coating_gap and ratios
-	self.action_space = spaces.Box(low = np.array([0.05,200,1.5]), 
-										high = np.array([0.1,500,5]))
+		self.action_space = spaces.Box(low = np.array([0.05,200,1.5]),high = np.array([0.1,500,5]))
 
 	def judge(self,action):
 
@@ -75,9 +75,9 @@ Class SlotDie(gym.Env):
 	def reset(self): 
 	# return a value self.observation_space	
 
-    self.experiments_count = 0
-    self.observation = 0
-    return self.observation
+    	self.experiments_count = 0
+    	self.observation = 0
+    	return self.observation
 
 
 	def step(self,action):
